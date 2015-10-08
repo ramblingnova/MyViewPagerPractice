6fb13d8e148b104815e58de6153de290c74e731f
John Doe <johndoe@example.com>
2015-10-08 17:35:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/myviewpagerpractice/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tacademy.myviewpagerpractice;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    MyPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = (ViewPager)findViewById(R.id.pager);
+        mAdapter = new MyPagerAdapter();
+        pager.setAdapter(mAdapter);
+    }
+}
